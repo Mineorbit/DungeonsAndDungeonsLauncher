09b44abaaae101968617d0fd80becf1b27d37003
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-06-17 01:18:54 +0200

    fixed downloading of files

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,45 @@
 <script lang="ts">
 	export let name: string;
-	let statusButton = "Download"
 	let progress = 0
+	let gamePresent = false
 	let testvalue = ""
-	import { appDir } from '@tauri-apps/api/path';
-	import { createDir, BaseDirectory } from '@tauri-apps/api/fs';
+	import { appDir, normalize, resolve } from '@tauri-apps/api/path';
+	import { createDir, BaseDirectory, writeBinaryFile, BinaryFileContents} from '@tauri-apps/api/fs';
 	import { readDir } from '@tauri-apps/api/fs';
-	import { fetch } from '@tauri-apps/api/http';
+	import { fetch, Response, ResponseType } from '@tauri-apps/api/http';
+	import { Command } from '@tauri-apps/api/shell'
 	async function downloadGame()
 	{
 		await createDir('game',{dir: BaseDirectory.App,recursive: true});
-		const response = await fetch('http://46.232.248.108/', {
-  method: 'GET',
-  timeout: 30,
-});
+		await createDir('download',{dir: BaseDirectory.App,recursive: true});
+		await decompressGame();
+		console.log("Downloading File");
+		var response = await fetch('http://46.232.248.108/Win64.zip', {
+  			method: 'GET',
+  			timeout: 300,
+  			responseType: ResponseType.Binary});
+		writeBinaryFile('download/Win64.zip',response.data,{dir: BaseDirectory.App});
+		
+		
 	}
 
+	async function decompressGame()
+	{
+		var appDirPath = await appDir()
+		
+		var downloadpath = await resolve(appDirPath, 'download', 'Win64.zip');
+		var runpath = await resolve(appDirPath, 'game');
+		console.log(downloadpath)
+		console.log(runpath)
+		var cmd = new Command('unzip',[downloadpath,"-d "+runpath]).execute();
+		console.log("unzipped");
+		gamePresent = true
+	}
+
+	async function runGame()
+	{
+
+	}
 
 
 </script>
@@ -23,7 +47,7 @@
 <main>
 	<div>
 	<progress value={progress} max="100"></progress>
-	<button on:click={downloadGame}>{statusButton}</button></div>
+	<button on:click={gamePresent?runGame:downloadGame}>{gamePresent?"Run":"Download"}</button></div>
 </main>
 
 <style>
