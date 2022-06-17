cb3410301d45f7656ed1506e736e9539c6e94da1
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-06-17 03:21:17 +0200

    basic launcher now works for windows

--- src/App.svelte
+++ src/App.svelte
@@ -8,18 +8,22 @@
 	import { readDir } from '@tauri-apps/api/fs';
 	import { fetch, Response, ResponseType } from '@tauri-apps/api/http';
 	import { Command } from '@tauri-apps/api/shell'
+			// With the Tauri API npm package:
+import { invoke } from '@tauri-apps/api/tauri'
+// With the Tauri global script, enabled when `tauri.conf.json > build > withGlobalTauri` is set to true:
+
+
 	async function downloadGame()
 	{
 		await createDir('game',{dir: BaseDirectory.App,recursive: true});
 		await createDir('download',{dir: BaseDirectory.App,recursive: true});
-		await decompressGame();
-		console.log("Downloading File");
+		console.log("Downloading Game");
 		var response = await fetch('http://46.232.248.108/Win64.zip', {
   			method: 'GET',
   			timeout: 300,
   			responseType: ResponseType.Binary});
-		writeBinaryFile('download/Win64.zip',response.data,{dir: BaseDirectory.App});
-		
+		await writeBinaryFile('download/Win64.zip',response.data,{dir: BaseDirectory.App});
+		await decompressGame();
 		
 	}
 
@@ -31,23 +35,31 @@
 		var runpath = await resolve(appDirPath, 'game');
 		console.log(downloadpath)
 		console.log(runpath)
-		var cmd = new Command('unzip',[downloadpath,"-d "+runpath]).execute();
-		console.log("unzipped");
+// Invoke the command
+		await invoke('execCommandExtra', { invokeMessage: 'unzip', arg: downloadpath, arg2: runpath })
+		
+		
+		//var cmd = new Command("unzip",[downloadpath,"-d",runpath]);
+		console.log("Unzipped Game Files");
 		gamePresent = true
 	}
 
 	async function runGame()
 	{
-
+		var appDirPath = await appDir()
+		var runPath = await resolve(appDirPath, 'game', 'DungeonsAndDungeons.exe');
+		
+		invoke('execCommand', { invokeMessage: "powershell", arg: runPath })
 	}
 
 
 </script>
 
 <main>
-	<div>
-	<progress value={progress} max="100"></progress>
+	<div id="controls">
+	<progress value={progress} max="100" style="width: 80%;"></progress>
 	<button on:click={gamePresent?runGame:downloadGame}>{gamePresent?"Run":"Download"}</button></div>
+	
 </main>
 
 <style>
@@ -56,6 +68,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	#controls {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
 	}
 
 	h1 {
