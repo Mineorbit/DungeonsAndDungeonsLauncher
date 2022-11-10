bf3c627db544fac3f2a55376409ada8278eb9b4e
mineorbit <mineorbitbeta@gmail.com>
2022-11-10 22:08:23 +0100

    bumped to 1.2.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,22 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn update_repo(path: &str) {
+    use git2::Repository;
+    print!("Test");
+    let url = "https://github.com/Mineorbit/DungeonsAndDungeons";
+    let repo = match Repository::clone(url, path) {
+    Ok(repo) => repo,
+    Err(e) => panic!("failed to clone: {}", e),
+    };
 }
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![update_repo])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
   import { createDir, BaseDirectory } from '@tauri-apps/api/fs';
+  import { invoke } from '@tauri-apps/api/tauri'
   import Greet from "$lib/Greet.svelte";
-    import { start_hydrating } from 'svelte/internal';
+  import { start_hydrating } from 'svelte/internal';
+
+  import { appDataDir } from '@tauri-apps/api/path';
   var godotdownload = "https://downloads.tuxfamily.org/godotengine/4.0/beta4/Godot_v4.0-beta4_win64.exe.zip";
   
   async function open()
   {
     console.log("Started installing Game")
     await createDir('gameSource', { dir: BaseDirectory.App, recursive: true });
+    var localDataDirPath = await appDataDir();
+    //var path
+    console.log(localDataDirPath);
+   // await invoke('update_repo', { path:"path" });
   }
 </script>
 
